2c03354e057f58dccecf67b78eb74342f0f0ddd9
Anderson Cruz <67594396+AnderCruz@users.noreply.github.com>
2025-10-29 21:22:12 +0000

    commit

--- app.py
+++ app.py
@@ -3,26 +3,26 @@ from tensorflow import keras
 import matplotlib.pyplot as plt
 import streamlit as st
 
-def plot_images(imagens):
+def plot_images(images):
     plt.figure(figsize=(20, 20))
-    for i in range(len(imagens)):
-        ax = plt.subplot(1, len(imagens), i + 1)
-        plt.imshow(imagens[i])
+    for i in range(len(images)):
+        ax = plt.subplot(1, len(images), i + 1)
+        plt.imshow(images[i])
         plt.axis("off")
-    st.pyplot(plt.gcf())  # Exibe a figura no Streamlit
+    st.pyplot(plt.gcf())  # Displays the figure in Streamlit
 
 def main():
-    prompt = st.text_input('Digite a descrição da imagem em inglês')  # Corrigido o nome da função
-    estilo = '''dark fantasy art,
+    prompt = st.text_input('Enter the image description in English')  # Corrected function name
+    style = '''dark fantasy art,
     high quality, highly detailed, elegant, sharp focus,
     concept art, character concepts, digital painting, mystery, adventure'''
     
     keras.mixed_precision.set_global_policy("mixed_float16")
-    modelo = keras_cv.models.StableDiffusion(img_width=512, img_height=512)
+    model = keras_cv.models.StableDiffusion(img_width=512, img_height=512)
     
     if prompt:
-        imagens = modelo.text_to_image(prompt + estilo, batch_size=1)  # batch_size especificado
-        plot_images(imagens)  # Exibe as imagens
+        images = model.text_to_image(prompt + style, batch_size=1)  # Specified batch_size
+        plot_images(images)  # Displays the images
 
 if __name__ == "__main__":
     main()
